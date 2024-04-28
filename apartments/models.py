2fc781e0d435700b9b5b7c0e77087802744d53a7
motaz ahmed Rawi <motazalrawi2002@gmail.com>
2024-04-29 00:05:49 +0300

    #11 added apartment list view

--- apartments/models.py
+++ apartments/models.py
@@ -22,10 +22,12 @@ class Category(models.Model):
 
 class Apartment(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    country=models.ForeignKey(Country,on_delete=models.SET_NULL,null=True,blank=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     apt_name = models.CharField(max_length=255)
     description = models.TextField()
-    district = models.IntegerField()
+    cover_image=models.ImageField(upload_to='covers/',null=True)
+    district = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
     is_booked = models.BooleanField(default=False)
     rooms = models.IntegerField()
@@ -35,6 +37,15 @@ class Apartment(models.Model):
     tv = models.BooleanField(default=False)
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
 
+
     def __str__(self):
         return self.apt_name
+
+
+class ApartmentImage(models.Model):
+    apartment = models.ForeignKey(Apartment, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='apartment_images/')
+
+    def __str__(self):
+        return f"Image for {self.apartment.apt_name}"
 
--- apartments/views.py
+++ apartments/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Apartment,ApartmentImage
 #import list view
 from django.views.generic import ListView, DetailView 
 
 # Create your views here.
-def index(request):
-    return render(request, 'apartment/index.html') 
+class ApartmentListView(ListView):
+    model = Apartment
+    context_object_name = 'apartments'
+    template_name = "apartment/list.html"
+
+  
